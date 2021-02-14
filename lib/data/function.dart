f505aaf0a4a81d1092a8e7a8e10ed7ae775e54dd
eruns001 <50986002+eruns001@users.noreply.github.com>
2021-02-14 22:55:17 +0900

    -ing login

--- lib/data/function.dart
+++ lib/data/function.dart
@@ -1,4 +1,30 @@
+import 'dart:developer';
+
 import 'package:image_picker/image_picker.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
+FirebaseAuth auth = FirebaseAuth.instance;
+final gooleSignIn = GoogleSignIn();
 
+Future<bool> googleSingIn () async{
+  GoogleSignInAccount googleSignInAccount = await gooleSignIn.signIn();
 
+  if( googleSignInAccount != null){
+    GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
+
+    AuthCredential credential = GoogleAuthProvider.getCredential(
+        idToken: googleSignInAuthentication.idToken,
+        accessToken: googleSignInAuthentication.accessToken
+    );
+
+    AuthResult result = await auth.signInWithCredential(credential);
+
+    FirebaseUser user = await auth.currentUser();
+    log("user.uid : ${user.uid}");
+    return Future.value(true);
+
+    //집가자.
+    //https://youtu.be/LLmg_n-EdWg?t=658
+  }
+}
