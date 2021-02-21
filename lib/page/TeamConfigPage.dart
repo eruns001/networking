3bff88c25bd7c03d58ef467aa7a0e1543993927b
juhwan976 <juhwan976@gmail.com>
2021-02-21 23:54:26 +0900

    팀설정 페이지 스크롤바 추가

--- lib/page/TeamConfigPage.dart
+++ lib/page/TeamConfigPage.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+/// 위젯을 하나 만들었는데, Stateful 로 하는것이 귀찮아서 Function 을 받아서 거기에
+/// setState 를 넣는쪽으로 했는데, 불필요한 반복이 너무 많아서 Stateful 로 바꿀까 고민중.
+
 class TeamConfigPage extends StatefulWidget {
   const TeamConfigPage({Key key}) : super(key: key);
 
@@ -14,6 +17,8 @@ class _TeamConfigPageState extends State<TeamConfigPage> {
   TextEditingController _teamNameController = new TextEditingController();
   TextEditingController _subjectController = new TextEditingController();
 
+  ScrollController _listViewScrollController = new ScrollController();
+
   int _planningMemberNum = 1;
   int _designMemberNum = 1;
   int _marketingMemberNum = 1;
@@ -21,179 +26,189 @@ class _TeamConfigPageState extends State<TeamConfigPage> {
 
   /// 페이지 내용 빌드 메서드
   Widget _buildPage(double _deviceHeight, double _deviceWidth) {
-    return Container(
-      child: ListView(
-        children: <Widget>[
-          Container(
-            /// 팀 이름
-            margin: EdgeInsets.fromLTRB(0, _deviceHeight * 0.021, 0, 0),
-            alignment: Alignment.center,
-            child: Text('팀 이름'),
-          ),
-          TeamConfigTextField(
-            /// 팀 이름 입력 공간
-            deviceHeight: _deviceHeight,
-            deviceWidth: _deviceWidth,
-            controller: _teamNameController,
-            onSubmitted: (String string) {
-              _team.teamName = string;
-            },
-          ),
-          Container(
-            /// 프로젝트 주제
-            margin: EdgeInsets.fromLTRB(0, _deviceHeight * 0.011, 0, 0),
-            alignment: Alignment.center,
-            child: Text('프로젝트 주제'),
-          ),
-          TeamConfigTextField(
-            /// 프로젝트 주제 입력 공간
-            deviceHeight: _deviceHeight,
-            deviceWidth: _deviceWidth,
-            controller: _subjectController,
-            onSubmitted: (String string) {
-              _team.subject = string;
-            },
-          ),
-          Container(
-            /// 분할선
-            margin: EdgeInsets.fromLTRB(0, _deviceHeight * 0.01, 0, 0),
-            child: Divider(),
-          ),
-          TeamConfigTemplate(
-            /// 기획
-            deviceHeight: _deviceHeight,
-            deviceWidth: _deviceWidth,
-            title: '기획',
-            memberNum: _planningMemberNum,
-            memberList: _team.planning,
-            editMember: () {},
-            addMember: () {
-              _team.planning.add(
-                Member(
-                  name: 'test',
-                  phoneNum: '000-0000-0000',
-                  position: 'null',
-                  email: 'test@naver.com',
-                ),
-              );
+    return NotificationListener<OverscrollIndicatorNotification>(
+      onNotification: (OverscrollIndicatorNotification overScroll) {
+        overScroll.disallowGlow();
+        return;
+      },
+      child: Container(
+        child: Scrollbar(
+          controller: _listViewScrollController,
+          child: ListView(
+            controller: _listViewScrollController,
+            children: <Widget>[
+              Container(
+                /// 팀 이름
+                margin: EdgeInsets.fromLTRB(0, _deviceHeight * 0.021, 0, 0),
+                alignment: Alignment.center,
+                child: Text('팀 이름'),
+              ),
+              TeamConfigTextField(
+                /// 팀 이름 입력 공간
+                deviceHeight: _deviceHeight,
+                deviceWidth: _deviceWidth,
+                controller: _teamNameController,
+                onSubmitted: (String string) {
+                  _team.teamName = string;
+                },
+              ),
+              Container(
+                /// 프로젝트 주제
+                margin: EdgeInsets.fromLTRB(0, _deviceHeight * 0.011, 0, 0),
+                alignment: Alignment.center,
+                child: Text('프로젝트 주제'),
+              ),
+              TeamConfigTextField(
+                /// 프로젝트 주제 입력 공간
+                deviceHeight: _deviceHeight,
+                deviceWidth: _deviceWidth,
+                controller: _subjectController,
+                onSubmitted: (String string) {
+                  _team.subject = string;
+                },
+              ),
+              Container(
+                /// 분할선
+                margin: EdgeInsets.fromLTRB(0, _deviceHeight * 0.01, 0, 0),
+                child: Divider(),
+              ),
+              TeamConfigTemplate(
+                /// 기획
+                deviceHeight: _deviceHeight,
+                deviceWidth: _deviceWidth,
+                title: '기획',
+                memberNum: _planningMemberNum,
+                memberList: _team.planning,
+                editMember: () {},
+                addMember: () {
+                  _team.planning.add(
+                    Member(
+                      name: 'test',
+                      phoneNum: '000-0000-0000',
+                      position: 'null',
+                      email: 'test@naver.com',
+                    ),
+                  );
 
-              setState(() {
-                _planningMemberNum += 1;
-              });
-            },
-          ),
-          Container(
-            margin: EdgeInsets.fromLTRB(
-              _deviceWidth * 0.055,
-              _deviceHeight * 0.037,
-              _deviceWidth * 0.055,
-              0,
-            ),
-            child: Divider(),
-          ),
-          TeamConfigTemplate(
-            /// 디자인
-            deviceHeight: _deviceHeight,
-            deviceWidth: _deviceWidth,
-            title: '디자인',
-            memberNum: _designMemberNum,
-            memberList: _team.design,
-            editMember: () {},
-            addMember: () {
-              _team.design.add(
-                Member(
-                  name: 'test',
-                  phoneNum: '000-0000-0000',
-                  position: 'null',
-                  email: 'test@naver.com',
+                  setState(() {
+                    _planningMemberNum += 1;
+                  });
+                },
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(
+                  _deviceWidth * 0.055,
+                  _deviceHeight * 0.037,
+                  _deviceWidth * 0.055,
+                  0,
                 ),
-              );
+                child: Divider(),
+              ),
+              TeamConfigTemplate(
+                /// 디자인
+                deviceHeight: _deviceHeight,
+                deviceWidth: _deviceWidth,
+                title: '디자인',
+                memberNum: _designMemberNum,
+                memberList: _team.design,
+                editMember: () {},
+                addMember: () {
+                  _team.design.add(
+                    Member(
+                      name: 'test',
+                      phoneNum: '000-0000-0000',
+                      position: 'null',
+                      email: 'test@naver.com',
+                    ),
+                  );
 
-              setState(() {
-                _designMemberNum += 1;
-              });
-            },
-          ),
-          Container(
-            margin: EdgeInsets.fromLTRB(
-              _deviceWidth * 0.055,
-              _deviceHeight * 0.037,
-              _deviceWidth * 0.055,
-              0,
-            ),
-            child: Divider(),
-          ),
-          TeamConfigTemplate(
-            /// 마케팅
-            deviceHeight: _deviceHeight,
-            deviceWidth: _deviceWidth,
-            title: '마케팅',
-            memberNum: _marketingMemberNum,
-            memberList: _team.marketing,
-            editMember: () {},
-            addMember: () {
-              _team.marketing.add(
-                Member(
-                  name: 'test',
-                  phoneNum: '000-0000-0000',
-                  position: 'null',
-                  email: 'test@naver.com',
+                  setState(() {
+                    _designMemberNum += 1;
+                  });
+                },
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(
+                  _deviceWidth * 0.055,
+                  _deviceHeight * 0.037,
+                  _deviceWidth * 0.055,
+                  0,
                 ),
-              );
+                child: Divider(),
+              ),
+              TeamConfigTemplate(
+                /// 마케팅
+                deviceHeight: _deviceHeight,
+                deviceWidth: _deviceWidth,
+                title: '마케팅',
+                memberNum: _marketingMemberNum,
+                memberList: _team.marketing,
+                editMember: () {},
+                addMember: () {
+                  _team.marketing.add(
+                    Member(
+                      name: 'test',
+                      phoneNum: '000-0000-0000',
+                      position: 'null',
+                      email: 'test@naver.com',
+                    ),
+                  );
 
-              setState(() {
-                _marketingMemberNum += 1;
-              });
-            },
-          ),
-          Container(
-            margin: EdgeInsets.fromLTRB(
-              _deviceWidth * 0.055,
-              _deviceHeight * 0.037,
-              _deviceWidth * 0.055,
-              0,
-            ),
-            child: Divider(),
-          ),
-          TeamConfigTemplate(
-            /// 개발
-            deviceHeight: _deviceHeight,
-            deviceWidth: _deviceWidth,
-            title: '개발',
-            memberNum: _developmentMemberNum,
-            memberList: _team.development,
-            editMember: () {},
-            addMember: () {
-              _team.development.add(
-                Member(
-                  name: 'test',
-                  phoneNum: '000-0000-0000',
-                  position: 'null',
-                  email: 'test@naver.com',
+                  setState(() {
+                    _marketingMemberNum += 1;
+                  });
+                },
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(
+                  _deviceWidth * 0.055,
+                  _deviceHeight * 0.037,
+                  _deviceWidth * 0.055,
+                  0,
                 ),
-              );
+                child: Divider(),
+              ),
+              TeamConfigTemplate(
+                /// 개발
+                deviceHeight: _deviceHeight,
+                deviceWidth: _deviceWidth,
+                title: '개발',
+                memberNum: _developmentMemberNum,
+                memberList: _team.development,
+                editMember: () {},
+                addMember: () {
+                  _team.development.add(
+                    Member(
+                      name: 'test',
+                      phoneNum: '000-0000-0000',
+                      position: 'null',
+                      email: 'test@naver.com',
+                    ),
+                  );
 
-              setState(() {
-                _developmentMemberNum += 1;
-              });
-            },
+                  setState(() {
+                    _developmentMemberNum += 1;
+                  });
+                },
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(
+                  _deviceWidth * 0.3925,
+                  _deviceHeight * 0.037,
+                  _deviceWidth * 0.3925,
+                  _deviceHeight * 0.055,
+                ),
+                height: _deviceHeight * 0.03,
+                width: _deviceWidth * 0.217,
+                child: FlatButton(
+                  padding: EdgeInsets.zero,
+                  child: Text('저장'),
+                  onPressed: () {},
+                ),
+              ),
+            ],
           ),
-          Container(
-            margin: EdgeInsets.fromLTRB(
-              _deviceWidth * 0.3925,
-              _deviceHeight * 0.037,
-              _deviceWidth * 0.3925,
-              _deviceHeight * 0.055,
-            ),
-            height: _deviceHeight * 0.03,
-            width: _deviceWidth * 0.217,
-            child: FlatButton(
-              padding: EdgeInsets.zero,
-              child: Text('저장'),
-              onPressed: () {},
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
